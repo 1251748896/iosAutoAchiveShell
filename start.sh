6c969573ff8d2d0b87f9be1ab4d92658eeaa183a
jiangbo <1251748896@qq.com>
2021-07-28 12:48:48 +0800

    增加一些输出语句

--- start.sh
+++ start.sh
@@ -79,25 +79,30 @@ fi
 exportOptionsConfig="${ios_auto_achive}/exportOption/${ExportOptionsPath}/ExportOptions.plist"
 
 # 调整打包配置文件
+echo "开始: 调整打包配置"
 ./fix_export_option.sh ${kehuTeamId}
-echo "调整打包配置"
+echo "调整打包配置---------完成"
 
 # 清空achives和ipas
+echo "开始: 清空上次打包文件"
 ./clean_data.sh ${achiveDirPath} ${exportIpaPath}
-echo "清空上次打包文件"
+echo "清空上次打包文件---------完成"
 
 # 则自动调整git分支
 # ./fix_branch.sh ${RNPath} ${role}
-
+echo "检查: 替换客户账号和包名"
 if [ ${appId} != ${kehuAppId} ]
 then 
-echo "替换客户账号和包名"
+echo "正在替换客户账号和包名..."
 ./replace_info.sh ${appId} ${kehuAppId} ${teamId} ${kehuTeamId} ${iosPorjectPath}
 fi
+echo "替换客户账号和包名---------完成"
 
-
+echo "开始: export_achive"
 ./export_achive.sh ${achive_mode} ${projectName} ${exportAchivePath} ${iosPorjectPath}
+echo "export_achiveq---------完成"
+echo "开始: export_ipa"
 ./export_ipa.sh ${ipa_mode} ${exportAchivePath} ${exportIpaPath} ${exportOptionsConfig}
-
+echo "export_ipa---------完成"
 # ./upload.sh ${ipaType} ${appstore} ${adhoc} ${exportIpaPath} ${projectName} ${apiKey} ${apiIssuer}
 
