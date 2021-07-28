fccd9c8ef2b8eb858e3934c9861b0ff9ba1f727e
jiangbo <1251748896@qq.com>
2021-07-28 12:44:59 +0800

    针对账号id和密钥的一些修改

--- ios_auto_achive.sh
+++ ios_auto_achive.sh
@@ -1,24 +1,18 @@
 # ios_auto_achive.sh
-
+role=$1
 
 kehudomain="com.huochaoduo"
-# role="logistics"
-role="driver"
-# admin
-# kehuTeamId="8LHYU4Q64W"
-# apiKey="5GBHM7MZ32"
-# apiIssuer="4ade02e5-e2cb-48f9-b2df-a4df103677fd"
-# 139
-kehuTeamId="6PU7HJG232"
-apiKey="VL2HR59X8L"
-apiIssuer="69a6de97-b5c3-47e3-e053-5b8c7c11a4d1"
+
+kehuTeamId="9SX98DB3MS"
+apiKey="T84F3MJX42"
+apiIssuer="cf484402-b74b-424e-90a6-8cbd83faa6bd"
 
 # ipaType(打包类型): 1: appstore; 2: adhoc
 ipaType="2"
 
-# ./start.sh ${kehudomain} "logistics" ${kehuTeamId} ${ipaType} ${apiKey} ${apiIssuer}
+./start.sh ${kehudomain} ${role} ${kehuTeamId} ${ipaType} ${apiKey} ${apiIssuer}
 
-./start.sh ${kehudomain} "driver" ${kehuTeamId} ${ipaType} ${apiKey} ${apiIssuer}
+# ./start.sh ${kehudomain} "driver" ${kehuTeamId} ${ipaType} ${apiKey} ${apiIssuer}
 
 
 
--- start.sh
+++ start.sh
@@ -15,6 +15,26 @@ adhoc="2"
 achive_mode=Release
 ipa_mode=Debug
 
+defaultrole="hcddriver"
+
+if [ ${role} == "" ]
+then
+echo "打包司机端"
+role=${defaultrole} 
+else 
+echo "打包物流端"
+fi
+
+projectName=""
+if [ ${role} == ${defaultrole} ]
+then
+projectName="LogisticsDriver"
+else 
+projectName="LogisticsOwner"
+fi
+
+
+
 if [ ${ipaType} == ${appstore} ]
 then ipa_mode=Release
 fi
@@ -24,17 +44,23 @@ appId="com.huochaoduo.${role}"
 # 客户的 kehuAppId
 kehuAppId="${kehudomain}.${role}"
 # 需要被替换的 teamId
-teamId="6PU7HJG232"
-
-projectName="HuoChaoDuo"
+teamId="9SX98DB3MS"
 
 # 脚本文件夹（ios_auto_achive）和RN代码文件夹（app），最好在平级目录（在同一个文件夹当中）
 
 desktopPath="/Users/jiangbo/Desktop"
-ios_auto_achive="${desktopPath}/ios_auto_achive"
-RNPath="${desktopPath}/app"
+ios_auto_achive="${desktopPath}/打包脚本/ios_auto_achive"
+
+projectPath=""
+if [ ${role} == ${defaultrole} ]
+then
+projectPath="${desktopPath}/driver_ios/LogisticsDriver"
+else 
+projectPath="${desktopPath}/logistics_ios/LogisticsOwner"
+fi
+
 # ios工程目录
-iosPorjectPath="${RNPath}/ios"
+iosPorjectPath="${projectPath}"
 # achive的路径
 achiveDirPath="${ios_auto_achive}/achives"
 # ipa的路径
@@ -73,5 +99,5 @@ fi
 ./export_achive.sh ${achive_mode} ${projectName} ${exportAchivePath} ${iosPorjectPath}
 ./export_ipa.sh ${ipa_mode} ${exportAchivePath} ${exportIpaPath} ${exportOptionsConfig}
 
-./upload.sh ${ipaType} ${appstore} ${adhoc} ${exportIpaPath} ${projectName} ${apiKey} ${apiIssuer}
+# ./upload.sh ${ipaType} ${appstore} ${adhoc} ${exportIpaPath} ${projectName} ${apiKey} ${apiIssuer}
 
