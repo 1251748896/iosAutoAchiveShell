1e7533b525dd2c0519606760600de5a397a366f7
jiangbo <1251748896@qq.com>
2021-07-30 09:49:50 +0800

    解开测试注释

--- start.sh
+++ start.sh
@@ -77,7 +77,7 @@ echo "调整打包配置---------完成"
 
 # 清空achives和ipas
 echo "开始: 清空上次打包文件"
-# ./clean_data.sh ${achiveDirPath} ${exportIpaPath}
+./clean_data.sh ${achiveDirPath} ${exportIpaPath}
 echo "清空上次打包文件---------完成"
 
 # 则自动调整git分支
@@ -91,11 +91,11 @@ fi
 echo "替换客户账号和包名---------完成"
 
 echo "开始: export_achive"
-# ./export_achive.sh ${achive_mode} ${projectName} ${exportAchivePath} ${iosPorjectPath}
+./export_achive.sh ${achive_mode} ${projectName} ${exportAchivePath} ${iosPorjectPath}
 echo "export_achiveq---------完成"
 echo "开始: export_ipa"
-# ./export_ipa.sh ${ipa_mode} ${exportAchivePath} ${exportIpaPath} ${exportOptionsConfig}
+./export_ipa.sh ${ipa_mode} ${exportAchivePath} ${exportIpaPath} ${exportOptionsConfig}
 echo "export_ipa---------完成"
-# ./upload.sh ${ipaType} ${appstore} ${adhoc} ${exportIpaPath} ${projectName} ${apiKey} ${apiIssuer}
+./upload.sh ${ipaType} ${appstore} ${adhoc} ${exportIpaPath} ${projectName} ${apiKey} ${apiIssuer}
 
 ./uploadToFir.sh ${ipaType} ${appstore} ${adhoc} ${exportIpaPath} ${projectName} ${apiKey} ${apiIssuer} ${appId}
