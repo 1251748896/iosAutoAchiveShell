d454e7e6ae0b5eb1d32339ba2b9bee652e26807b
jiangbo <1251748896@qq.com>
2021-10-12 11:26:27 +0800

    flutter司机端打包调试

--- flutter.sh
+++ flutter.sh
@@ -3,30 +3,17 @@
 
 kehudomain=$1
 role=$2
-
 kehuTeamId=$3
 ipaType=$4
 apiKey=$5
 apiIssuer=$6
-
 defaultrole=$7
 
+projectName="Runner"
 appstore="1"
 adhoc="2"
-
 achive_mode=Release
 ipa_mode=Debug
-
-projectName=""
-if [ ${role} == ${defaultrole} ]
-then
-projectName="LogisticsDriver"
-else 
-projectName="Runner"
-fi
-
-echo ${projectName}
-
 if [ ${ipaType} == ${appstore} ]
 then ipa_mode=Release
 fi
@@ -37,22 +24,20 @@ appId="com.huochaoduo.${role}"
 kehuAppId="${kehudomain}.${role}"
 # 需要被替换的 teamId
 teamId="9SX98DB3MS"
-
-# 脚本文件夹（ios_auto_achive）和RN代码文件夹（app），最好在平级目录（在同一个文件夹当中）
-
 desktopPath="/Users/kuotian/Desktop"
+
 ios_auto_achive="${desktopPath}/打包脚本/ios_auto_achive"
 
+
+# ios工程目录
 projectPath=""
 if [ ${role} == ${defaultrole} ]
 then
-projectPath="${desktopPath}/driver_ios/LogisticsDriver"
+projectPath="${desktopPath}/app/ios"
 else 
 projectPath="${desktopPath}/app/ios"
 fi
 
-# ios工程目录
-iosPorjectPath="${projectPath}"
 # achive的路径
 achiveDirPath="${ios_auto_achive}/achives"
 # ipa的路径
@@ -70,28 +55,22 @@ fi
 
 exportOptionsConfig="${ios_auto_achive}/exportOption/${ExportOptionsPath}/ExportOptions.plist"
 
-# 调整打包配置文件(.plist文件)
-echo "开始: 调整打包配置"
-./fix_export_option.sh ${kehuTeamId}
-echo "调整打包配置---------完成"
-
 # 清空achives和ipas
 echo "开始: 清空上次打包文件"
 ./clean_data.sh ${achiveDirPath} ${exportIpaPath}
 echo "清空上次打包文件---------完成"
 
-# 则自动调整git分支
-# ./fix_branch.sh ${RNPath} ${role}
 echo "检查: 替换客户账号和包名"
 if [ ${appId} != ${kehuAppId} ]
 then 
 echo "正在替换客户账号和包名..."
-./replace_info.sh ${appId} ${kehuAppId} ${teamId} ${kehuTeamId} ${iosPorjectPath}
+./fix_export_option.sh ${kehuTeamId}
+./replace_info.sh ${appId} ${kehuAppId} ${teamId} ${kehuTeamId} ${projectPath}
 fi
 echo "替换客户账号和包名---------完成"
 
 echo "开始: export_achive"
-./export_achive.sh ${achive_mode} ${projectName} ${exportAchivePath} ${iosPorjectPath}
+./export_achive.sh ${achive_mode} ${projectName} ${exportAchivePath} ${projectPath}
 echo "export_achiveq---------完成"
 echo "开始: export_ipa"
 ./export_ipa.sh ${ipa_mode} ${exportAchivePath} ${exportIpaPath} ${exportOptionsConfig}
